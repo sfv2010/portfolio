c4bab3e7064fe5917e2db139c298b856d12f0a3e
Sayaka Vincent <sfv2010@hotmail.fr>
2024-06-21 13:12:12 +0200

    ✨ Add Footer

--- scripts/works.js
+++ scripts/works.js
@@ -2,29 +2,46 @@ document.addEventListener("DOMContentLoaded", function () {
     const worksContainer = document.getElementById("works-container");
 
     const items = [
-        { title: "Portfolio1", imageUrl: "img/mado1.jpg" },
-        { title: "Portfolio2", imageUrl: "img/mado1.jpg" },
-        { title: "Portfolio3", imageUrl: "img/mado1.jpg" },
-        { title: "Portfolio4", imageUrl: "img/mado1.jpg" },
+        {
+            title: "Site d'échange de cours de français et de japonais",
+            imageUrl: "img/bubble2.png",
+            imageAlt: "site avec photo de france et japon",
+        },
+        {
+            title: "Site du café",
+            imageUrl: "img/sayacafe.png",
+            imageAlt: "site avec photo de café",
+        },
+        {
+            title: "Application de gestion des employés",
+            imageUrl: "img/image.png",
+            imageAlt: "site avec formulaire",
+        },
+
+        {
+            title: "Site du restaurant",
+            imageUrl: "img/ohmy.png",
+            imageAlt: "site avec photo de plat",
+        },
     ];
 
     items.forEach((item) => {
         const worksItem = document.createElement("div");
-        worksItem.classList.add("works__item", "mb-md");
+        worksItem.classList.add("works__item");
 
         const coverSlide = document.createElement("div");
         coverSlide.classList.add("coverSlide", "hoverDarken");
 
         const img = document.createElement("img");
         img.src = item.imageUrl;
-        img.alt = "";
+        img.alt = item.imageAlt;
         img.classList.add("imgZoom");
 
         coverSlide.appendChild(img);
         worksItem.appendChild(coverSlide);
 
         const worksTitle = document.createElement("p");
-        worksTitle.classList.add("works__title", "font-md");
+        worksTitle.classList.add("works__title");
         worksTitle.textContent = item.title;
 
         worksItem.appendChild(worksTitle);
