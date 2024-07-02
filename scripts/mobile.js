3cdd7be15c9e97e4ada374309f25b9370da8912f
Sayaka Vincent <sfv2010@hotmail.fr>
2024-07-02 11:12:00 +0200

    💄 Fix menuMobile__cover

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -6,6 +6,7 @@ class MobileMenu {
         this.DOM.container = document.querySelector("#globalContainer");
         this.eventType = this._getEventType();
         this._addEvent();
+        this._adjustCoverHeight(); // カバーの高さを調整する
     }
 
     _getEventType() {
@@ -17,12 +18,16 @@ class MobileMenu {
     }
     _toggle() {
         this.DOM.container.classList.toggle("header__menuOpen");
+        this._adjustCoverHeight(); // メニューの状態が変わるたびにカバーの高さを再調整する
     }
 
     //スマホで見られたときはタッチ、PCはクリックと切り替える
     _addEvent() {
         this.DOM.btn.addEventListener("click", this._toggle.bind(this));
         this.DOM.cover.addEventListener("click", this._toggle.bind(this));
+    }
+    _adjustCoverHeight() {
+        this.DOM.cover.style.height = `${document.documentElement.scrollHeight}px`;
     }
 }
 
